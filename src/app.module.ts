65af7d6d775586293944c7460d34638db8679f61
ShaggyRobot <shaggyrobot@gmail.com>
2023-05-17 19:37:16 +0300

    exclude Logger module in app.module as filesystem on cyclic is read-only writing to fs causes exceptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { LoggerModule } from './logger/logger.module';
+// import { LoggerModule } from './logger/logger.module';
 import { UsersModule } from './resources/users/users.module';
 import { BoardsModule } from './resources/boards/boards.module';
 import { TasksModule } from './resources/tasks/tasks.module';
@@ -24,7 +24,7 @@ import ormconfig from './ormconfig';
       entities: [`${__dirname}/resources/**/**.entity{.ts,.js}`],
       migrations: [`${__dirname}/migrations/*.ts`],
     }),
-    LoggerModule,
+    // LoggerModule,
     UsersModule,
     BoardsModule,
     ColumnsModule,
